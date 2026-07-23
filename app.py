75129ebf70112cffd93e30f18def7eb4453dfc17
MARCOS <marcos733tuto@gmail.com>
2026-07-23 00:43:55 -0300

    Implementacao do banco de dados

--- app.py
+++ app.py
@@ -1,57 +1,8 @@
-# 1. Objetivo - Fornecer todos os serviços necessários para o sistema de gerenciamento de cuidadores de idosos, permitindo que administradores, cuidadores e familiares acessem e manipulem informações de forma segura.
+from flask import Flask
+from routes.usuarios  import usuario_bp
 
-# 2. URL base - localhost/API/V1
+app = Flask(__name__)
 
-# 3. Endpoints -
-    AUTENTIÇAO
-        POST   / login
-        POST   / logout
-        POST   / resfresh-token
+app.register_blueprint(usuario_bp)
 
-    USUARIOS
-        GET    / users
-        GET    / users/{id}
-        POST   / users
-        PUT    / users/{id}
-        DELETE / users/{id}
-
-    EMPRESAS 
-        GET     / empresas
-        GET     / empresas/{id}
-        POST    / empresas
-        PUT     / empresas/{id}
-        DELETE  / empresas/{id}
-
-    POCIENTES
-        GET     / pacientes
-        GET     / pacientes/{id}
-        POST    / pacientes
-        PUT     / pacientes/{id}
-        DELETE  / pacientes/{id}
-
-    CUIDADORES
-        GET     / cuidadores
-        GET     / cuidadores/{id}
-        POST    / cuidadores
-        PUT     / cuidadores/{id}
-        DELETE  / cuidadores/{id}
-
-    FAMILIARES
-        GET     / familiares
-        GET     / familiares/{id}
-        POST    / familiares
-        PUT     / familiares/{id}
-        DELETE  / familiares/{id}
-
-    RELATORIOS
-        GET     / relatorios
-        POST    / relatorios
-        GET     / relatorios/{id}
-        PUT     / relatorios/{id}
-        DELETE  / relatorios/{id}
-
-    PONTOS
-        POST    / pontos
-        PUT     / pontos/{id}/saida
-        GET     / pontos
-        GET     / pontos/{id}
+app.run(host="localhost", port=5000, debug=True)
--- database/connect.py
+++ database/connect.py
@@ -1,4 +1,6 @@
 import sqlite3
 
 def connect():
-    return sqlite3.connect("database/homecare.b")
+    conexao =  sqlite3.connect("database/homecare.db")
+    conexao.row_factory = sqlite3.Row
+    return conexao
