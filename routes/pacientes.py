0cd2225a8d9a6d80da18a23b57677030eba116b7
MARCOS <marcos.fagundes@gmail.com>
2026-08-04 14:11:12 -0300

    consulta de todos os pacientes

--- routes/pacientes.py
+++ routes/pacientes.py
@@ -8,44 +8,84 @@ import sqlite3
 pacientes_bp = Blueprint("pacientes", __name__)
 
 # CRIANDO PACIENTE 
-@pacientes_bp.route("/pacientes", methods=['POST'])
+@pacientes_bp.route("/pacientes/criar", methods=['POST'])
 @jwt_required()
 @admin_required()
 def criar_pacientes():
-    dados = request.get_json()
 
-    if not dados:
+    conexao = None
+
+    try:
+        dados = request.get_json()
+
+        if not dados:
+            return jsonify({
+                'erro': 'Dados não enviados'
+            }), 400
+        
+        conexao = connect()
+        cursor = conexao.cursor()
+
+        cursor.execute("""
+        INSERT INTO pacientes
+        (
+            usuario_id,
+            cpf,
+            data_nascimento,
+            tel,
+            endereco,
+            obs
+        )
+
+        VALUES (?, ?, ?, ?, ?, ?)
+        """,(
+            dados["usuario_id"],
+            dados["cpf"],
+            dados["data_nascimento"],
+            dados["tel"],
+            dados["endereco"],
+            dados["obs"]
+        ))
+
+        conexao.commit()
+
         return jsonify({
-            'erro': 'Dados não enviados'
+            'msg': 'Paciente cadastrado com sucesso'
+        }), 201
+    
+    except sqlite3.IntegrityError:
+        return jsonify({
+            "erro": "CPF já cadastrado."
         }), 400
 
-    conexao = connect()
-    cursor = conexao.cursor()
+    finally: 
+        if conexao:
+            conexao.close()
 
-    cursor.execute("""
-    INSERT INTO pacientes
-    (
-        usuario_id,
-        cpf,
-        data_nascimento,
-        tel,
-        endereco,
-        obs
-    )
+# PEGANDO TODOS PACIENTES
+@pacientes_bp.route("/pacientes", methods=['GET'])
+@jwt_required()
+@admin_required()
+def mostrar_pacientes():
 
-    VALUES (?, ?, ?, ?, ?, ?)
-    """,(
-        dados["usuario_id"],
-        dados["cpf"],
-        dados["data_nascimento"],
-        dados["tel"],
-        dados["endereco"],
-        dados["obs"]
-    ))
+    try: 
+        conexao = connect()
+        cursor = conexao.cursor()
 
-    conexao.commit()
-    conexao.close()
+        cursor.execute("SELECT * FROM pacientes")
 
-    return jsonify({
-        'msg': 'Paciente cadastrado com sucesso'
-    }), 201
+        pacientes = cursor.fetchall()
+
+        lista_pacientes = []
+
+        for paciente in pacientes:
+            dados = dict(paciente)
+            lista_pacientes.append(dados)
+
+        conexao.close()
+
+        return jsonify (lista_pacientes), 200
+
+    finally:
+        if conexao: 
+            conexao.close()
